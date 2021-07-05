a68d2e07420c401dcbc2bc4b69b6072064a6dad7
insiders@000111 <77982282+beginnertodesign@users.noreply.github.com>
2021-07-05 08:01:40 +0530

    Update md5_hashing.py

--- md5_hashing.py
+++ md5_hashing.py
@@ -1,3 +1,4 @@
+#This is md5-hashing algorithm
 import hashlib
 
 username1 = input()
@@ -10,3 +11,62 @@ md5_ref_2 = hashlib.md5(store2)
 
 print(md5_ref_1.hexdigest())
 print(md5_ref_2.hexdigest())
+
+
+
+
+
+#This is a sha1 hashing algorithm
+import hashlib
+
+username1 = input()
+store1 = bytes(username1,'utf-8')
+username2 = input()
+store2 = bytes(username2,'utf-8')
+
+md5_ref_1 = hashlib.sha1(store1)
+md5_ref_2 = hashlib.sha1(store2)
+
+print(md5_ref_1.hexdigest())
+print(md5_ref_2.hexdigest())
+
+#sha224 hashing technique
+import hashlib
+
+username1 = input()
+store1 = bytes(username1,'utf-8')
+username2 = input()
+store2 = bytes(username2,'utf-8')
+
+md5_ref_1 = hashlib.sha224(store1)
+md5_ref_2 = hashlib.sha224(store2)
+
+print(md5_ref_1.hexdigest())
+print(md5_ref_2.hexdigest())
+
+
+#blake2s hashing algorithm
+import hashlib
+
+username1 = input()
+store1 = bytes(username1,'utf-8')
+username2 = input()
+store2 = bytes(username2,'utf-8')
+
+md5_ref_1 = hashlib.blake2s(store1)
+md5_ref_2 = hashlib.blake2s(store2)
+
+print(md5_ref_1.hexdigest())
+print(md5_ref_2.hexdigest())
+
+
+
+
+#salting to the data and doing final hashing with sha512
+import hashlib, uuid
+
+userinput = input()
+password = bytes(userinput,'utf-8')
+salt = uuid.uuid4().bytes
+hashed_password = hashlib.sha512(password + salt).hexdigest()
+print(hashed_password)
