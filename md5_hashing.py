0f03c2f78b7206772c2ca78b4273c76df3052758
insiders@000111 <77982282+beginnertodesign@users.noreply.github.com>
2021-07-05 07:40:57 +0530

    Create md5_hashing.py

---
+++ md5_hashing.py
@@ -0,0 +1,12 @@
+import hashlib
+
+username1 = input()
+store1 = bytes(username1,'utf-8')
+username2 = input()
+store2 = bytes(username2,'utf-8')
+
+md5_ref_1 = hashlib.md5(store1)
+md5_ref_2 = hashlib.md5(store2)
+
+print(md5_ref_1.hexdigest())
+print(md5_ref_2.hexdigest())
