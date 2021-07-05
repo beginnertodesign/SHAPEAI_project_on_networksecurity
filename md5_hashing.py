782da6e27b8c2e2b56a915cc01cbb011af6c3f90
insiders@000111 <77982282+beginnertodesign@users.noreply.github.com>
2021-07-05 08:09:22 +0530

    Update md5_hashing.py

--- md5_hashing.py
+++ md5_hashing.py
@@ -16,11 +16,3 @@ print(md5_ref_2.hexdigest())
 
 
 
-#salting to the data and doing final hashing with sha512
-import hashlib, uuid
-
-userinput = input()
-password = bytes(userinput,'utf-8')
-salt = uuid.uuid4().bytes
-hashed_password = hashlib.sha512(password + salt).hexdigest()
-print(hashed_password)
